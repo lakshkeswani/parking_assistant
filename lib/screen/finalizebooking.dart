d049a7ed4a769e2cb45e1bad075b653f990d2a9a
laksh <lakshkeswani3@gmail.com>
2021-05-25 21:45:01 +0500

    location working

---
+++ lib/screen/finalizebooking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinalizeBooking extends StatefulWidget {
+  const FinalizeBooking({Key key}) : super(key: key);
+
+  @override
+  _FinalizeBookingState createState() => _FinalizeBookingState();
+}
+
+class _FinalizeBookingState extends State<FinalizeBooking> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
